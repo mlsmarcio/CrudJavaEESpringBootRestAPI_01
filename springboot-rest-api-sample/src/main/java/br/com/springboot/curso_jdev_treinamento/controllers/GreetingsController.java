138e63c6d5c04d81def7118e6b6e91db55a79f21
= <mlsmarciogm@gmail.com>
2021-08-06 22:55:57 -0300

    Criação do projeto Maven, especificação da base de dados no application.properties, criação do Usuario model, repository e teste no Controllers

---
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -0,0 +1,47 @@
+package br.com.springboot.curso_jdev_treinamento.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.springboot.curso_jdev_treinamento.model.Usuario;
+import br.com.springboot.curso_jdev_treinamento.repository.UsuarioRepository;
+
+/**
+ *
+ * A sample greetings controller to return greeting text
+ */
+@RestController
+public class GreetingsController {
+	
+	@Autowired /* IC/CD ou CDI - Injeção de dependência */
+	private UsuarioRepository usuarioRepository;
+	
+    /**
+     *
+     * @param name the name to greet
+     * @return greeting text
+     */
+    //@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    @RequestMapping(value = "/mostrarnome/{name}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String greetingText(@PathVariable String name) {
+        return "Curso Spring Boot API: " + name + "!";
+    }
+    
+    // MAPEAMENTO DE REQUISIÇÃO
+    @RequestMapping(value = "/salvausuario/{nome}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)   // RESPOSTA PADRÃO PARA A REQUISIÇÃO
+    public String exibeMensagem(@PathVariable String nome) {
+    	Usuario usuario = new Usuario();
+    	usuario.setNome(nome);
+    	
+    	usuarioRepository.save(usuario);
+    	
+    	return "Salvou " + nome;
+    }
+}

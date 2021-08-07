aee12d2dc355ecc20d46d52b304960fd6dcf7a22
= <mlsmarciogm@gmail.com>
2021-08-06 23:37:37 -0300

    Adicionando o método listatodos da API

--- springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -1,10 +1,15 @@
 package br.com.springboot.curso_jdev_treinamento.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,5 +48,12 @@ public class GreetingsController {
     	usuarioRepository.save(usuario);
     	
     	return "Salvou " + nome;
+    }
+    
+    @GetMapping(value = "listatodos")
+    @ResponseBody /* retorna os dados no corpo da resposta*/  
+    public ResponseEntity<List<Usuario>> listaUsuario(){
+    	List<Usuario> usuarios = usuarioRepository.findAll(); /* spring data executa a consulta no bd*/
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK); /* retorna no formato json*/
     }
 }
